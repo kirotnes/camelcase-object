6a7a548613ef48b2a07557e0c18dd4d110d0f675
Bob Axford <bob@whooshkaa.com>
2019-10-22 14:58:14 +1100

    test for 2d array

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 'use strict';
 var mapObj = require('map-obj');
 var camelCase = require('camelcase');
-const util = require('util')
+
+const mapFunc = function (key, val) {
+    return [camelCase(key), (Array.isArray(val) ? 
+        mapArray(val)
+        :  
+        (val === Object(val) && !Array.isArray(val)) ? mapObj(val,mapFunc) : val)];
+}    
+
+const mapArray = (val) => {
+    return val.map(function(o){
+        return Array.isArray(o) ? mapArray(o)
+        : (typeof(o) === 'object') ? mapObj(o, mapFunc)
+        : o
+    }) 
+}
 
 module.exports = function (obj) {
-	var mapFunc = function (key, val) {
-        return [camelCase(key), (Array.isArray(val) ? 
-            val.map(function(o){
-                return (typeof(o) === 'object' || Array.isArray(o)) ?  mapObj(o, mapFunc) : o
-            }) 
-            :  
-            (val === Object(val) && !Array.isArray(val)) ? mapObj(val,mapFunc) : val)];
-	}    
     return mapObj(obj, mapFunc);
 };
